0abbe4a119484fee9073dd7f3e90e78e9575b777
Stefan Bedorf <stefan.bedorf@otrs.com>
2026-04-25 18:21:37 +0200

    Fix sudo-rs config change in Ubuntu 26.04.

--- packer_templates/ubuntu/scripts/sudoers.sh
+++ packer_templates/ubuntu/scripts/sudoers.sh
@@ -1,7 +1,18 @@
 #!/bin/sh -eux
 
-sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers;
+# Get the major version (like "26" from the version string (like "26.04")
+major_version=$(lsb_release -r | cut -f 2 | cut -d . -f 1)
 
 # Set up password-less sudo for the vagrant user
-echo 'vagrant ALL=(ALL) NOPASSWD:ALL' >/etc/sudoers.d/99_vagrant;
+if [ -n "$major_version" -a "$major_version" -lt 26 ] ; then
+    sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers;
+    echo 'vagrant ALL=(ALL) NOPASSWD:ALL' >/etc/sudoers.d/99_vagrant;
+else
+    cat - <<EOF > /etc/sudoers.d/99_vagrant
+Defaults:%sudo !secure_path
+Defaults:%sudo env_keep += "PATH"
+vagrant ALL=(ALL) NOPASSWD:ALL
+EOF
+fi
+
 chmod 440 /etc/sudoers.d/99_vagrant;
